fce35f22a33cfa8a7ad0af9d6e68fdf43c1711d5
phaalonso <26033899+phaalonso@users.noreply.github.com>
2021-04-29 22:07:50 -0300

    Corrigindo socket.on('error')

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -53,19 +53,30 @@ server.on('connection', socket => {
 	})
 })
 
+let count = 0;
+
 server.on('error', err => {
-	if (err.name === 'EADDINUSE') {
+	//@ts-ignore
+	if (err.code === 'EADDRINUSE') {
+		count++;
+
+		if (count > 5) {
+			process.exit(1);
+		}
+
 		console.log('Endereço já está em uso, tentando novamente...');
 		setTimeout(() => {
 			server.close();
 			server.listen(PORT, HOST);
-		}, 1000);
+		}, 1000 * count);
+	} else {
+		console.log(err);
 	}
-
 });
 
 const cpu = osu.cpu;
 const ram = osu.mem;
+//const drive = osu.drive;
 
 function sendCpu() {
 	cpu.usage().then(cpu => {
@@ -79,6 +90,15 @@ function sendRam() {
 	});
 }
 
+//function sendDriveUsage() {
+	//// Como conseguir o nome do drive?
+	//drive.info().then(data => {
+		//publish('drive', `drive_free_${data.freeGb}`);
+		//publish('drive', `drive_used_${data.usedGb}`);
+
+	//});
+//}
+
 server.listen(PORT, HOST, () => {
 	console.log(`Servidor iniciado em`, server.address());
 
@@ -87,4 +107,5 @@ server.listen(PORT, HOST, () => {
 	
 	setInterval(sendCpu, 1000);
 	setInterval(sendRam, 1000);
+	//setInterval(sendDiskUsage, 1000);
 });
