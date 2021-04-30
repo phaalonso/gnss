81b63129e5dc0ceefa32e5005f81cee63c7cf4b5
phaalonso <26033899+phaalonso@users.noreply.github.com>
2021-04-30 14:27:58 -0300

    WIP

--- src/database/mongodb/prnindices.ts
+++ src/database/mongodb/prnindices.ts
@@ -1,8 +1,28 @@
+import { PrnInfoModel } from "./prnindices/prnindices.model";
+
 export class PrnIndicesMongo {
 	insertProcessedData(dpSnr: number, s4: number, time: Date, prn: number) {
 		//return this.dao.run(
-			//'INSERT INTO prnindices (prn, mediasnr, mediaazi, mediaelev, tinicial, tfinal, dpsnr, s4) SELECT prn, AVG(snr), AVG(azi), AVG(elev), min(time), max(time), ?, ? from prninfo where time between ?-60000 and ? and prn = ? group by prn',
-			//[dpSnr, s4, time, time, prn]
+		//'INSERT INTO prnindices (prn, mediasnr, mediaazi, mediaelev, tinicial, tfinal, dpsnr, s4) SELECT prn, AVG(snr), AVG(azi), AVG(elev), min(time), max(time), ?, ? from prninfo where time between ?-60000 and ? and prn = ? group by prn',
+		//[dpSnr, s4, time, time, prn]
 		//);
+
+		const agregate = PrnInfoModel.aggregate()
+			.match({
+				time: {
+					$lte: time,
+					$gt: new Date(time.getTime() - 1000 * 60),
+				},
+			})
+			.group({
+				_id: "$prn",
+				avgSnr: { $avg: "$snr" },
+				avgAzi: { $avg: "$azi" },
+				avgElev: { $avg: "$elev" },
+				minTime: { $min: "$time" },
+				maxTime: { $max: "$time" },
+			});
+
+		console.log(agregate);
 	}
 }
