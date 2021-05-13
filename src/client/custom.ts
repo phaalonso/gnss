9407237946bf5b94b01e07671732215f2383e7fb
phaalonso <26033899+phaalonso@users.noreply.github.com>
2021-05-12 23:41:15 -0300

    Cliente socket para consumir dados

--- src/client/custom.ts
+++ src/client/custom.ts
@@ -2,88 +2,108 @@ import net from "net";
 import { connect } from "../database/mongodb/connection";
 import { CustomData, processCustomData } from "../processData";
 
-let socket: net.Socket;
+export class Client {
+	//TODO restabelecer a conexão automaticamente
+	public socket: net.Socket;
+	private timeout;
+	private cb: Function;
 
-connect().then(() => {
-	socket = net.createConnection({ port: 2108 }, () => { console.log('Conectado com o servidor!');
-	});
+	public active = false;
 
-	socket.on('connect', () => {
-		console.log('Conexão estabelecida');
-	})
+	constructor() {
+		this.socket = new net.Socket();
 
-	socket.on("lookup", (
-			err: Error, address: string, family: string | number, host: string ) => {
-			if (err) {
-				console.log("Erro ao tentar criar conexão");
-				console.log(err);
-			} else {
-				console.log("Conexão criada");
-			}
-		}
-	);
+		this.socket.on("connect", () => {
+			console.log("Conexão estabelecida");
+			//TODO criar buffer de mensagens
+			this.socket.write('sub_custom');
+		});
 
-	socket.on("data", (data) => {
-		const msgs = data.toString().split("\n");
+		this.socket.on('data', data => {
+			const msgs = data.toString().split("\n");
+			console.log(msgs)
 
-		for (const msg of msgs) {
-			const matchRec = msg.match(/^rec_(.*)_(.*)$/);
+			for (const msg of msgs) {
+				const matchRec = msg.match(/^rec_(.*)_(.*)$/);
 
-			if (matchRec && matchRec[1] && matchRec[2]) {
-				//TODO Marcar que a mensagem enviada foi recebida pelo cliente
+				if (matchRec && matchRec[1] && matchRec[2]) {
+					//TODO Marcar que a mensagem enviada foi recebida pelo cliente
+					console.log(msg);
+					return;
+				}
+
+				//`sat_prn_snr_azimuth_elevation_lat_lon_time.getTime()\n`)
+				const matchCustom = msg.match( /^sat_(.*)_(.*)_(.*)_(.*)_(.*)_(.*)_(.*)$/);
+				//console.log(matchCustom[7]);
+
+				if ( matchCustom && matchCustom[1] && matchCustom[2] && matchCustom[3] && matchCustom[4] && matchCustom[5] && matchCustom[6] && matchCustom[7]) {
+					const snr = parseFloat(matchCustom[2]) || null;
+					//console.log(msg, snr);
+					const customData: CustomData = {
+						prn: parseInt(matchCustom[1]),
+						snr: snr,
+						azimuth: parseFloat(matchCustom[3]),
+						elevation: parseFloat(matchCustom[4]),
+						lat: parseFloat(matchCustom[5]),
+						lon: parseFloat(matchCustom[6]),
+						time: new Date(parseInt(matchCustom[7])),
+					};
+
+					console.log(customData);
+
+					processCustomData(customData);
+
+					return;
+				}
+
 				console.log(msg);
-				return;
 			}
 
-			//`sat_prn_snr_azimuth_elevation_lat_lon_time.getTime()\n`)
-			const matchCustom = msg.match(
-				/^sat_(.*)_(.*)_(.*)_(.*)_(.*)_(.*)_(.*)$/
-			);
-			//console.log(matchCustom[7]);
+		});
 
-			if (
-				matchCustom &&
-				matchCustom[1] &&
-				matchCustom[2] &&
-				matchCustom[3] &&
-				matchCustom[4] &&
-				matchCustom[5] &&
-				matchCustom[6] &&
-				matchCustom[7]
-			) {
-				const snr = parseFloat(matchCustom[2]) || null;
-				//console.log(msg, snr);
-				const customData: CustomData = {
-					prn: parseInt(matchCustom[1]),
-					snr: snr,
-					azimuth: parseFloat(matchCustom[3]),
-					elevation: parseFloat(matchCustom[4]),
-					lat: parseFloat(matchCustom[5]),
-					lon: parseFloat(matchCustom[6]),
-					time: new Date(parseInt(matchCustom[7])),
-				};
+		this.socket.on("error", (err) => {
+			console.log("Erro na conexão socket");
+			console.log(err);
+			this.active = false;
+		});
 
-				//console.log(customData);
+		this.socket.on("end", () => {
+			console.log("End");
+			this.active = false;
+		});
+	}
 
-				processCustomData(customData);
+	run(cb?: Function): net.Socket {
+		this.cb = cb;
 
-				return;
+		this.socket.on( "lookup", ( err: Error, address: string, family: string | number, host: string) => {
+				if (err) {
+					console.log("Erro ao tentar criar conexão");
+					console.log(err);
+				} else {
+					console.log("Conexão criada");
+				}
 			}
+		);
 
-			console.log(msg);
-		}
-	});
+		return this.socket.connect({ port: 2108 }, () => {
+			this.active = true;
 
-	socket.on("error", (err) => {
-		console.log('Erro na conexão socket')
+			if (cb)
+				return cb();
+			return;
+		});
+	}
+}
+
+if (require.main == module) {
+	connect().then(() => {
+		const client = new Client();
+
+		client.run(() => {
+			console.log('Client is running');
+		})
+	}).catch((err) => {
 		console.log(err);
 	});
-
-	socket.on("end", () => {
-		console.log("End");
-	});
-}).catch(err => {
-	console.log(err);
-});
-
-export default socket;
+}
