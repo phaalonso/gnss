2be50ff5e5b8bee0042f4cd0b19b6a9083c7cee4
phaalonso <26033899+phaalonso@users.noreply.github.com>
2021-05-20 22:02:35 -0300

    Movendo tudo para backend

--- setup.sh
+++
@@ -1,2 +0,0 @@
-chmod 666 /dev/ttyUSB0
-stty -F /dev/ttyUSB0 115200
--- src/config/gpsConfig.ts
+++
@@ -1,9 +0,0 @@
-interface GpsConfig {
-	serialInput: string;
-	baudRage: number;
-}
-
-export const GPSConfig: GpsConfig = {
-	serialInput: '/dev/ttyUSB0',
-	baudRage: 115200
-}
