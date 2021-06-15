c06b3f31486ccd0b2e0c0402ae71ca4411b4f6a4
phaalonso <26033899+phaalonso@users.noreply.github.com>
2021-06-15 12:33:03 -0300

    Entrada de dados podem ser originadas a partir de outras streams

--- client/src/mongoClient.ts
+++ client/src/mongoClient.ts
@@ -11,8 +11,8 @@ if (require.main == module) {
 		const prnIndices = new PrnIndicesMongo()
 		const processData = new ProcessData(prnInfo, prnIndices);
 
-		const client = new Client(processData,  { port: 3000, host: '192.168.3.23' });
-		// const client = new Client(processData,  { port: 2108, host: 'localhost' });
+		//const client = new Client(processData,  { port: 3000, host: '192.168.3.23' });
+		 const client = new Client(processData,  { port: 2108, host: 'localhost' });
 
 		client.run(() => {
 			logger.log('Client is running');
